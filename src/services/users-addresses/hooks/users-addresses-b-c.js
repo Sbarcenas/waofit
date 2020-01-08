2640f8130a465240b90c49d2d9d3f202b2148db9
Leonel Gomez Gomez <inf.leonel@gmail.com>
2020-01-08 11:15:48 -0500

    se pueden agragar las direcciones de los usuarios y marcarla como principal

--- src/services/users-addresses/hooks/users-addresses-b-c.js
+++ src/services/users-addresses/hooks/users-addresses-b-c.js
@@ -1,6 +1,7 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 const { checkContext, getItems, replaceItems } = require('feathers-hooks-common')
+const { NotFound } = require('@feathersjs/errors')
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
     return async context => {
@@ -10,6 +11,28 @@ module.exports = (options = {}) => {
         const { user } = context.params
 
         records.user_id = user.id
+
+        const state = await context.app.service('locations-states').getModel()
+            .query().where({
+                name: records.state_name,
+                deletedAt: null
+            }).then(it => it[0]);
+
+        if (!state) throw new NotFound('No se encontro el departamento enviado.')
+
+        const city = await context.app.service('locations-cities').getModel()
+            .query().where({
+                state_id: state.id,
+                name: records.city_name,
+                deletedAt: null
+            }).then(it => it[0]);
+
+        if (!city) throw new NotFound('No se encontro la ciudad enviada.')
+
+        records.city_id = city.id
+
+        delete records.city_name
+        delete records.state_name
 
         replaceItems(context, records)
 
