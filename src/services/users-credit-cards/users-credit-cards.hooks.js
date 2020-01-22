ea6cf01ee904f37fc76ac3d86a79575e42d10967
Leonel Gomez <inf.leonel@gmail.com>
2020-01-22 10:23:22 -0500

    se pueden agregar tarjetas de creditos

--- src/services/users-credit-cards/users-credit-cards.hooks.js
+++ src/services/users-credit-cards/users-credit-cards.hooks.js
@@ -1,5 +1,5 @@
 const usersCreditCardsBC = require('./hooks/users-credit-cards-b-c');
-
+const { disallow } = require('feathers-hooks-common')
 module.exports = {
   before: {
     all: [],
@@ -9,8 +9,12 @@ module.exports = {
       usersCreditCardsBC()
     ],
     update: [],
-    patch: [],
-    remove: []
+    patch: [
+      disallow('external')
+    ],
+    remove: [
+      disallow('external')
+    ]
   },
 
   after: {
