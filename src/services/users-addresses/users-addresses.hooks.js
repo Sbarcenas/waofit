027e2db0a156f497e3c486067ae4b53b9726aaf6
Leonel Gomez <inf.leonel@gmail.com>
2020-01-23 12:03:06 -0500

    se hizo un hook para softdelete para cuando hacen remove

--- src/services/users-addresses/users-addresses.hooks.js
+++ src/services/users-addresses/users-addresses.hooks.js
@@ -2,40 +2,19 @@ const usersAddressesBC = require('./hooks/users-addresses-b-c');
 const usersAddressesAC = require('./hooks/users-addreeses-a-c');
 const usersAddressesAP = require('./hooks/users-addresses-a-p');
 const validationOfFields = require('../../hooks/validations-of-fields');
-const { softDelete } = require('feathers-hooks-common')
-
-const deleted = softDelete({
-  // context is the normal hook context
-  deletedQuery: async context => {
-    return { deletedAt: null };
-  },
-  removeData: async context => {
-    return { deletedAt: new Date().toISOString() };
-  }
-});
 
 module.exports = {
   before: {
     all: [],
-    find: [
-      deleted
-    ],
-    get: [
-      deleted
-    ],
+    find: [],
+    get: [],
     create: [
       validationOfFields(),
       usersAddressesBC()
     ],
-    update: [
-      deleted
-    ],
-    patch: [
-      deleted
-    ],
-    remove: [
-      deleted
-    ]
+    update: [],
+    patch: [],
+    remove: []
   },
 
   after: {
