847e7a34ce2335734f61ad93a3f766456a1bb5ac
Leonel Gomez <inf.leonel@gmail.com>
2020-01-23 10:10:48 -0500

    se puede actualizar las cuotas por defecto de las tarjetas de creditos

--- src/services/users-credit-cards/hooks/default-credit-cards.js
+++ src/services/users-credit-cards/hooks/default-credit-cards.js
@@ -1,10 +1,6 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-const { checkContext, getItems, replaceItems } = require('feathers-hooks-common')
-const { NotAcceptable } = require('@feathersjs/errors');
-const users = require('../../../models/users.model');
-const Ajv = require('ajv');
-const ajv = new Ajv({ allErrors: true });
+const { getItems, replaceItems } = require('feathers-hooks-common')
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
