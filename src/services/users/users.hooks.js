3e26168f4dfd9303c6d6e41e6b30f41449582b8e
Leonel Gomez <inf.leonel@gmail.com>
2020-01-24 10:57:56 -0500

    se descartan campos a la hora de actualizar usuario,  como creditos

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -4,29 +4,21 @@ const proccessUsersFaceboookBC = require('./hooks/proccess-users-faceboook-b-c')
 const proccessUsersFacebookBP = require('./hooks/proccess-users-facebook-b-p');
 const proccessUsersBC = require('./hooks/proccess-users-b-c');
 const proccessFindFacebookBC = require('./hooks/proccess-find-facebook-b-c');
+const { discard, iff, isProvider } = require('feathers-hooks-common')
 const { softDelete } = require('feathers-hooks-common')
-
-
-const deleted = softDelete({
-  // context is the normal hook context
-  deletedQuery: async context => {
-    return { deletedAt: null };
-  },
-  removeData: async context => {
-    return { deletedAt: new Date().toISOString() };
-  }
-});
 
 module.exports = {
   before: {
-    all: [],
+    all: [
+      iff(isProvider('external'),
+        discard('credits','email','password','status','role','facebookId','token_reset_password')
+      )
+    ],
     find: [
-      deleted,
       authenticate('jwt'),
       proccessFindFacebookBC()
     ],
     get: [
-      deleted,
       authenticate('jwt'),
       proccessFindFacebookBC()
     ],
@@ -36,17 +28,14 @@ module.exports = {
       proccessUsersBC()
     ],
     update: [
-      deleted,
       hashPassword('password'),
       authenticate('jwt')
     ],
     patch: [
-      deleted,
       hashPassword('password'),
       authenticate('jwt') /* proccessUsersBC(), proccessUsersFacebookBP() */
     ],
     remove: [
-      deleted,
       authenticate('jwt')
     ]
   },
