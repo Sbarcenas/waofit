e77384525506b70400b35fe6481eb92aea3ca038
Leonel Gomez <inf.leonel@gmail.com>
2020-04-01 15:16:37 -0500

    se configura s3

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,7 @@
+const awsS3Middleware = require("./aws-s3-middleware");
 // eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
+module.exports = function(app) {
   // Add your custom middleware here. Remember that
+  app.use("/s3Client", awsS3Middleware());
   // in Express, the order matters.
 };
