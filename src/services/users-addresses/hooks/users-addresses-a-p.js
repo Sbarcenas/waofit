3a080992f48e04e273844e117da306df69cc79eb
Leonel Gomez <inf.leonel@gmail.com>
2020-04-02 10:04:07 -0500

    No se actualizan las direcciones de los demas usuarios

--- src/services/users-addresses/hooks/users-addresses-a-p.js
+++ src/services/users-addresses/hooks/users-addresses-a-p.js
@@ -1,34 +1,42 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-const { checkContext, getItems, replaceItems } = require('feathers-hooks-common')
+const {
+  checkContext,
+  getItems,
+  replaceItems
+} = require("feathers-hooks-common");
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-    return async context => {
+  return async context => {
+    let records = getItems(context);
 
-        let records = getItems(context)
+    const { user } = context.params;
 
-        const { user } = context.params
+    let main = null;
+    if (records.main == "true") {
+      main = await context.app
+        .service("users-addresses")
+        .getModel()
+        .query()
+        .select("*")
+        .where("main", "true")
+        .where("user_id", user.id);
+    }
 
-        let main = null;
-        if (records.main == 'true') {
+    if (main && main.length > 0) {
+      await context.app
+        .service("users-addresses")
+        .getModel()
+        .query()
+        .where({ user_id: user.id })
+        .whereNot("id", records.id)
+        .patch({
+          main: "false"
+        });
+    }
 
-            main = await context.app.service('users-addresses').getModel().query()
-                .select('*')
-                .where('main', 'true')
-                .where('user_id', user.id)
+    replaceItems(context, records);
 
-        }
-
-        if (main && main.length > 0) {
-            await context.app.service('users-addresses').getModel().query()
-                .whereNot('id', records.id)
-                .patch({
-                    main: 'false'
-                });
-        }
-
-        replaceItems(context, records)
-
-        return context;
-    };
+    return context;
+  };
 };
