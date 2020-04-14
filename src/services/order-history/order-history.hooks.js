dd265a1f56865e35e2f4627c2bc4375f98dde1d8
Leonel Gomez <inf.leonel@gmail.com>
2020-04-14 11:40:06 -0500

    se hace join con los estados en order history

--- src/services/order-history/order-history.hooks.js
+++ src/services/order-history/order-history.hooks.js
@@ -1,24 +1,39 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const { fastJoin } = require("feathers-hooks-common");
+
+const resolves = {
+  joins: {
+    join: () => async (records, context) => {
+      [records.order_status] = await Promise.all([
+        context.app
+          .service("orders-status")
+          .getModel()
+          .query()
+          .where({ id: records.order_status_id, deletedAt: null }),
+      ]);
+    },
+  },
+};
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
-
-  after: {
     all: [],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
+  },
+
+  after: {
+    all: [],
+    find: [fastJoin(resolves)],
+    get: [fastJoin(resolves)],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
   },
 
   error: {
@@ -28,6 +43,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
