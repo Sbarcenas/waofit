dd8e1eca9f49b5c36ac77b90aedc39e69196ef04
Leonel Gomez <inf.leonel@gmail.com>
2020-04-22 16:05:27 -0500

    se pueden crear favoritos y consultarlos

--- src/services/favorites/hooks/register-favorite.js
+++ src/services/favorites/hooks/register-favorite.js
@@ -1,13 +1,50 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 const { getItems, replaceItems } = require("feathers-hooks-common");
-const { NotAcceptable } = require("@feathersjs/errors");
+const { NotAcceptable, NotFound } = require("@feathersjs/errors");
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async (context) => {
     const records = getItems(context);
 
     const { user } = context.params;
+
+    let service = null;
+    let query = null;
+    switch (records.type) {
+      case "product":
+        service = "express-products";
+        query = { id: records.type_id, deletedAt: null };
+        break;
+      case "brand":
+        service = "brands";
+        query = { id: records.type_id, deletedAt: null };
+        break;
+      case "blogs":
+        service = "blogs-and-guides";
+        query = { id: records.type_id, deletedAt: null, type: "blog" };
+        break;
+      case "recipe":
+        service = "recipes";
+        query = { id: records.type_id, deletedAt: null };
+        break;
+      case "guide":
+        service = "blogs-and-guides";
+        query = { id: records.type_id, deletedAt: null, type: "guide" };
+        break;
+      default:
+        break;
+    }
+
+    const item = await context.app
+      .service(service)
+      .getModel()
+      .query()
+      .where(query)
+      .then((it) => it[0]);
+
+    if (!item)
+      throw new NotFound("No se encontró el item a marcar como favorito.");
 
     const favorite = await context.app
       .service("favorites")
