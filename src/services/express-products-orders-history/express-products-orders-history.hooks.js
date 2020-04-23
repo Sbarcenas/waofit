3c386409fcdfaf71462f6f39a82eb8ccef462102
Leonel Gomez <inf.leonel@gmail.com>
2020-04-23 11:32:27 -0500

    join de historial de detalles de pedidos

--- src/services/express-products-orders-history/express-products-orders-history.hooks.js
+++ src/services/express-products-orders-history/express-products-orders-history.hooks.js
@@ -1,4 +1,35 @@
+const { fastJoin } = require("feathers-hooks-common");
 
+const resolves = {
+  joins: {
+    join: () => async (records, context) => {
+      [
+        records.user,
+        records.order_status,
+        records.express_product_order,
+      ] = await Promise.all([
+        context.app
+          .service("users")
+          .getModel()
+          .query()
+          .select("first_name", "last_name", "email", "phone", "gender")
+          .where({ id: records.user_id })
+          .then((it) => it[0]),
+        context.app
+          .service("orders-status")
+          .getModel()
+          .query()
+          .where({ id: records.order_status_id }),
+        context.app
+          .service("express-products-orders")
+          .getModel()
+          .query()
+          .where({ id: records.express_product_order_id })
+          .then((it) => it[0]),
+      ]);
+    },
+  },
+};
 
 module.exports = {
   before: {
@@ -8,17 +39,17 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   after: {
     all: [],
-    find: [],
-    get: [],
+    find: [fastJoin(resolves)],
+    get: [fastJoin(resolves)],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +59,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
