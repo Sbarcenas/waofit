541b5d905021d6217e01ec1cedba39df6ec6b476
Leonel Gomez <inf.leonel@gmail.com>
2020-04-27 09:11:15 -0500

    se pueden agregar productos al detalle del carro de compras recurrente

--- src/services/recurring-shopping-cart-details/hooks/create-recurrung-shopping-cart-details.js
+++ src/services/recurring-shopping-cart-details/hooks/create-recurrung-shopping-cart-details.js
@@ -10,19 +10,27 @@ module.exports = (options = {}) => {
 
     const { user } = context.params;
 
+    const query =
+      user.role == "user"
+        ? {
+            id: records.recurring_shopping_cart_id,
+            user_id: user.id,
+            deletedAt: null,
+          }
+        : {
+            id: records.recurring_shopping_cart_id,
+            deletedAt: null,
+          };
+
     const recurringShoppingCart = await context.app
       .service("recurring-shopping-cart")
       .getModel()
       .query()
-      .where({
-        id: records.recurring_shopping_cart_id,
-        user_id: user.id,
-        deletedAt: null,
-      })
+      .where(query)
       .then((it) => it[0]);
 
     if (!recurringShoppingCart)
-      throw NotFound("No se encontró el carro de compras recurrente.");
+      throw new NotFound("No se encontró el carro de compras recurrente.");
 
     replaceItems(context, records);
 
