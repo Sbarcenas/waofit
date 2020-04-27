6848bee51648e34cf5511ae3d6e010b1ad827951
Leonel Gomez <inf.leonel@gmail.com>
2020-04-27 08:43:41 -0500

    se envian los joins de blogs y guias

--- src/services/blogs-and-guides/blogs-and-guides.hooks.js
+++ src/services/blogs-and-guides/blogs-and-guides.hooks.js
@@ -4,6 +4,54 @@ const updateAlgolia = require("./hooks/update-algolia");
 const restricActiveBeforePatch = require("./hooks/restric-active-before-patch");
 const removeSoftDelete = require("../../hooks/remove-softdelete");
 
+const { fastJoin } = require("feathers-hooks-common");
+
+const resolves = {
+  joins: {
+    join: () => async (records, context) => {
+      const [countCalifications, sumCalifications] = await Promise.all([
+        context.app
+          .service("reviews")
+          .getModel()
+          .query()
+          .count("*", { as: "quantity" })
+          .where({
+            type: "recipe",
+            type_id: records.id,
+            deletedAt: null,
+          }),
+        context.app
+          .service("reviews")
+          .getModel()
+          .query()
+          .sum("stars", { as: "totalCalifications" })
+          .where({
+            type: "recipe",
+            type_id: records.id,
+            deletedAt: null,
+          }),
+      ]);
+      records.rating_average =
+        parseInt(sumCalifications[0].totalCalifications) /
+        countCalifications[0].quantity;
+
+      records.rating_average = records.rating_average
+        ? records.rating_average
+        : 0;
+      records.count_reviews = countCalifications[0].quantity;
+
+      [records.author] = await Promise.all([
+        context.app
+          .service("authors")
+          .getModel()
+          .query()
+          .where({ id: records.author_id, deletedAt: null })
+          .then((it) => it[0]),
+      ]);
+    },
+  },
+};
+
 module.exports = {
   before: {
     all: [],
@@ -12,17 +60,17 @@ module.exports = {
     create: [registerBlogGuide()],
     update: [],
     patch: [restricActiveBeforePatch()],
-    remove: [removeSoftDelete()]
+    remove: [removeSoftDelete()],
   },
 
   after: {
     all: [],
-    find: [],
-    get: [],
+    find: [fastJoin(resolves)],
+    get: [fastJoin(resolves)],
     create: [],
     update: [],
     patch: [updateAlgolia()],
-    remove: [removeAlgolia()]
+    remove: [removeAlgolia()],
   },
 
   error: {
@@ -32,6 +80,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
