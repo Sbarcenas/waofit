3df3767f6018a958d94a8cb24527a972cc22b999
Leonel Gomez <inf.leonel@gmail.com>
2020-05-27 10:42:57 -0500

    joins de historiales de ordenes de cafeteria

--- src/services/coffee-orders-history/coffee-orders-history.hooks.js
+++ src/services/coffee-orders-history/coffee-orders-history.hooks.js
@@ -1,24 +1,46 @@
+const { disallow, fastJoin } = require("feathers-hooks-common");
 
+const resolves = {
+  joins: {
+    join: () => async (records, context) => {
+      [records.order_status, records.users] = await Promise.all([
+        context.app
+          .service("orders-status")
+          .getModel()
+          .query()
+          .where({ id: records.order_status_id, deletedAt: null })
+          .then((it) => it[0]),
+        context.app
+          .service("users")
+          .getModel()
+          .query()
+          .select("id", "first_name", "last_name", "email", "phone")
+          .where({ id: records.user_id, deletedAt: null })
+          .then((it) => it[0]),
+      ]);
+    },
+  },
+};
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [disallow("external")],
+    update: [disallow("external")],
+    patch: [disallow("external")],
+    remove: [disallow("external")],
   },
 
   after: {
     all: [],
-    find: [],
-    get: [],
+    find: [fastJoin(resolves)],
+    get: [fastJoin(resolves)],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +50,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
